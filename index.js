14cd9327241c61c62e84e09bc6db1076470ece32
Marvin <marvin.ogot@gmail.com>
2025-01-10 15:24:50 +0300

    String interpolation using template literals and also the simpler string concatenation

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 let currentUser = "Marvin Ogot";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreetings = `Welcome, ${currentUser.charAt(0)}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,3)}!`;
