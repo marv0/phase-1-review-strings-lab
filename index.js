7f4178bbab33d6f934526ff89ad2ec2b09956ac2
Marvin <marvin.ogot@gmail.com>
2025-01-10 16:49:09 +0300

    Contribution Issues

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreetings = `Welcome, ${currentUser.charAt(0)}`;
+const shortGreeting2 = `Welcome, ${currentUser.charAt(0)}`;
 
 const shortGreeting = `Welcome, ${currentUser.slice(0,3)}!`;
